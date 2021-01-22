0026fdd12eaa8744c7cfe4843c6e1d6bc9aae52c
sakeli <18337065203@163.com>
2021-01-22 16:51:53 +0800

    11111

--- 3.0project/src/App.vue
+++ 3.0project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>1111111111111111</h1>
+    <h1>11111111111111112222222222</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
