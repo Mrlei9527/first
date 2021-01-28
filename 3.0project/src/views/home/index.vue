788fec7e56c72bebe8c23f264cd2a3c1b2884af9
sakeli <18337065203@163.com>
2021-01-28 15:50:03 +0800

    增加首页

---
+++ 3.0project/src/views/home/index.vue
@@ -0,0 +1,22 @@
+<<template>
+    <div class=''>
+        hahahahahah
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="scope">
+    
+</style>
